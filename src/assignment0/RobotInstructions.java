3faeff94007b7a33193d44eeb44687c03bc6d3a6
bennikpour <bennikpour@bens-mbp.dhcp.wustl.edu>
2025-01-22 11:32:09 -0600

    Edited the robot to make a house

--- src/assignment0/RobotInstructions.java
+++ src/assignment0/RobotInstructions.java
@@ -29,11 +29,27 @@ public class RobotInstructions  {
 		 * You will add instructions at the end of the code to cause the support.robot to move
 		 * as you desire.
 		 */
-		robot.forward(60);
+		robot.forward(100);
 		robot.turnLeft(90);
-		robot.forward(23);
+		robot.forward(100);
+		robot.turnLeft(90);
+		robot.forward(100);
+		robot.turnLeft(90);
+		robot.forward(40);
+		robot.turnLeft(90);
+		robot.forward(25);
 		robot.turnRight(90);
-		robot.forward(80);
+		robot.forward(20);
+		robot.turnRight(90);
+		robot.forward(25);
+		robot.turnLeft(90);
+		robot.forward(40);
+		robot.turnLeft(90);
+		robot.forward(100);
+		robot.turnLeft(45);
+		robot.forward(70);
+		robot.turnLeft(90);
+		robot.forward(70);
 	}
 
 }
